84d802e7e58e330be22fbefda18ce47f1415fa0d
Roman Pituh <106968237+RomeoLeyn@users.noreply.github.com>
2026-08-01 15:34:56 +0300

    Updated settings page and added new method for update user profile (#9)

--- src/lib/services/user.service.ts
+++ src/lib/services/user.service.ts
@@ -1,9 +1,13 @@
 import { apiClient } from "@/src/lib/api-client";
-import { ChangePasswordDto, User } from "@/src/types";
+import { ChangePasswordDto, UpdateProfileDto, User } from "@/src/types";
 
 export const userService = {
-    async update(data: ChangePasswordDto) {
-        const response = await apiClient.patch<unknown>("/user/password", data);
-        console.log("Password updated successfully:", response);
+    async update(data: UpdateProfileDto) {
+        const response = await apiClient.patch<User>("/user/profile/update", data);
+        return response;
+    },
+
+    async changePassword(data: ChangePasswordDto) {
+        await apiClient.patch<void>("/user/password/update", data);
     },
 };
